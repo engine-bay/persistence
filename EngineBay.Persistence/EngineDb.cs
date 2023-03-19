1cfbd41ce1b171f071077e878b6045a682fc86c6
Matthew van der Velden <matthew.vdvelden@gmail.com>
2023-03-19 17:22:50 +0000

    #minor adds auditing

--- EngineBay.Persistence/EngineDb.cs
+++ EngineBay.Persistence/EngineDb.cs
@@ -1,8 +1,9 @@
 namespace EngineBay.Persistence
 {
+    using Audit.EntityFramework;
     using Microsoft.EntityFrameworkCore;
 
-    public class EngineDb : DbContext, IEngineDb
+    public class EngineDb : AuditDbContext, IEngineDb
     {
         public EngineDb(DbContextOptions options)
             : base(options)
