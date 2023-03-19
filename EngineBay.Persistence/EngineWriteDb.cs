a22c3bd38496e936a8920d565d8f7d9ca52d8e6a
Matthew van der Velden <matthew.vdvelden@gmail.com>
2023-03-19 20:19:05 +0000

    #minor reworks the auditing implementation

--- EngineBay.Persistence/EngineWriteDb.cs
+++ EngineBay.Persistence/EngineWriteDb.cs
@@ -23,7 +23,107 @@ namespace EngineBay.Persistence
         {
             this.SetTimeStamps();
 
-            return await base.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
+            // Get audit entries
+            var auditEntries = this.OnBeforeSaveChanges();
+
+            // Save current entity
+            var result = await base.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
+
+            // Save audit entries
+            await this.OnAfterSaveChangesAsync(auditEntries).ConfigureAwait(false);
+
+            return result;
+        }
+
+        private List<AuditEntry> OnBeforeSaveChanges()
+        {
+            this.ChangeTracker.DetectChanges();
+            var entries = new List<AuditEntry>();
+
+            foreach (var entry in this.ChangeTracker.Entries())
+            {
+                // Dot not audit entities that are not tracked, not changed, or not of type IAuditable
+                if (entry.State == EntityState.Detached || entry.State == EntityState.Unchanged || !(entry.Entity is BaseModel))
+                {
+                    continue;
+                }
+
+                if (entry.Properties is null)
+                {
+                    throw new ArgumentException();
+                }
+
+                var entityId = entry.Properties.Single(p => p.Metadata.IsPrimaryKey());
+
+                if (entityId is null)
+                {
+                    throw new ArgumentException();
+                }
+
+                var changes = entry.Properties.Select(p => new { p.Metadata.Name, p.CurrentValue });
+
+                if (changes is null)
+                {
+                    throw new ArgumentException();
+                }
+
+                var auditEntry = new AuditEntry
+                {
+                    ActionType = entry.State == EntityState.Added ? "INSERT" : entry.State == EntityState.Deleted ? "DELETE" : "UPDATE",
+                    EntityId = entityId.ToString(),
+                    EntityName = entry.Metadata.ClrType.Name,
+
+                    // Username = _username,
+                    // TimeStamp = DateTime.UtcNow,
+                    Changes = changes.ToDictionary(i => i.Name, i => i.CurrentValue),
+
+                    // TempProperties are properties that are only generated on save, e.g. ID's
+                    // These properties will be set correctly after the audited entity has been saved
+                    TempProperties = entry.Properties.Where(p => p.IsTemporary).ToList(),
+                };
+
+                entries.Add(auditEntry);
+            }
+
+            return entries;
+        }
+
+        private Task OnAfterSaveChangesAsync(List<AuditEntry> auditEntries)
+        {
+            if (auditEntries == null || auditEntries.Count == 0)
+            {
+                return Task.CompletedTask;
+            }
+
+            // For each temporary property in each audit entry - update the value in the audit entry to the actual (generated) value
+            foreach (var entry in auditEntries)
+            {
+                if (entry.TempProperties is null)
+                {
+                    throw new ArgumentException("Auditing temporary properties collection was null");
+                }
+
+                if (entry.Changes is null)
+                {
+                    throw new ArgumentException("Auditing change collection was null");
+                }
+
+                foreach (var prop in entry.TempProperties)
+                {
+                    if (prop.Metadata.IsPrimaryKey() && prop.CurrentValue is not null)
+                    {
+                        entry.EntityId = prop.CurrentValue.ToString();
+                        entry.Changes[prop.Metadata.Name] = prop.CurrentValue;
+                    }
+                    else
+                    {
+                        entry.Changes[prop.Metadata.Name] = prop.CurrentValue;
+                    }
+                }
+            }
+
+            this.AuditEntries.AddRange(auditEntries);
+            return this.SaveChangesAsync();
         }
 
         private void SetTimeStamps()
