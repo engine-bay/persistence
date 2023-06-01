2f3ad0e309271b5853ed213faef806a76e1b5485
Matthew van der Velden <matthew.vdvelden@entlect.co.uk>
2023-06-01 10:30:05 +0100

    #minor improved default sqlite file extension

--- EngineBay.Persistence/Constants/DefaultConnectionStringConstants.cs
+++ EngineBay.Persistence/Constants/DefaultConnectionStringConstants.cs
@@ -2,7 +2,7 @@ namespace EngineBay.Persistence
 {
     public static class DefaultConnectionStringConstants
     {
-        public const string DefaultSqliteConnectiontring = "Data Source=enginebay.db;Cache=Shared";
+        public const string DefaultSqliteConnectiontring = "Data Source=enginebay.sqlite;Cache=Shared";
         public const string DefaultInMemoryConnectiontring = "DataSource=file::memory:?cache=shared";
     }
 }
